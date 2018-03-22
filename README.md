42fca0f0aacde178f29a6dc84a604a645d8bb0ff
baijunjie <625603381@qq.com>
2018-03-22 22:58:49 +0900

    v1.3.0

--- README.md
+++ README.md
@@ -1,28 +1,27 @@
-# progress.js
+# browser-progress
 
 一款小巧的伪进度条插件，会在页面顶部伪装一个进度条，就好像 [YouTube](https://www.youtube.com/) 正在使用的那种。
-支持 IE6-8 等老式浏览器。
+
+<img src="https://github.com/baijunjie/browser-progress/blob/master/youtube-loading.jpg" width="640">
+
+
 
 ## Usage
 
-将 [progress.js] 引在 \<body\> 标签之后。<br>
-默认情况下 progress 会监听页面的 load 事件，当 load 事件触发时完成进程。<br>
+将 `BrowserProgress.js` 放置在 `<body\>` 标签之后。
 
-注意，如果页面还未触发 load 事件，那么此时调用任何进程相关的操作，都会将 load 事件监听移除，为的是防止 load 影响新的进程。
+默认情况下 BrowserProgress 会监听页面的 `load` 事件，当 `load` 事件触发时完成进程。
+
+**注意**，如果页面还未触发 `load` 事件，那么此时调用任何进程相关的操作，都会将 `load` 事件监听移除，为的是防止 `load` 影响新的进程。
 
 ``` html
 <body>
-<script src="progress.js"></script>
+<script src="BrowserProgress.js"></script>
 	.....
 	.....
 </body>
 ```
-[progress.js]: https://github.com/baijunjie/progress.js/blob/master/progress.js
 
-
-## AMD
-
-该插件同时支持模块化调用，但是通过模块化调用时，progress 不会监听页面的 load 事件，也不会在初加入页面时自动启动进程。
 
 ## npm
 
@@ -30,44 +29,50 @@
 $ npm install browser-progress
 ```
 
+**注意**，插件同时支持模块化调用，但是通过模块化调用时，由于 BrowserProgress 是异步载入的，此时页面的 `document.readyState` 可能已经为 `complete`，因此页面在初始载入时 BrowserProgress 可能不会自动启动进程。
+
+
+
 ## API
 
-如果不通过模块化加载，那么可以通过全局对象 progress 进行调用。
+如果不通过模块化加载，那么可以通过全局对象 BrowserProgress 进行调用。
 
 ```js
-// 修改进度条的容器，将进度条添加进指定的DOM元素中
-// 第2个参数可以帮助修改进度条的样式，一般只是修改它的position
-progress.appendTo(elem, style);
+// 第一个参数为一个 DOM 对象，可以修改进度条的容器，将进度条移动至指定的 DOM 元素中
+// 第二个参数是一个 Object, 可以修改进度条的样式，一般只是用来修改它的 position 属性
+BrowserProgress.appendTo(elem, style);
 
-progress.color("#000"); // 修改进度条颜色
+BrowserProgress.color("#000"); // 修改进度条颜色
 
-progress.set(.5); // 设置进程的当前值，取值为 0~1 的浮点数。同时会暂停运行中的进程，使进程变为手动设置
+BrowserProgress.set(.5); // 设置进程的当前值，取值为 0~1 的浮点数。同时会暂停运行中的进程，使进程变为手动设置
 
-progress.stop(); //	暂停进程
+BrowserProgress.stop(); //	暂停进程
 
-progress.play(); // 恢复运行进程
+BrowserProgress.play(); // 恢复运行进程
 
-progress.start(); // 重新开始进程
+BrowserProgress.start(); // 重新开始进程
 
-progress.done(); // 完成进程
+BrowserProgress.done(); // 完成进程
 
-progress.fail(); // 进程回零，一般用于加载失败
+BrowserProgress.fail(); // 进程回零，一般用于加载失败
 
-progress.hide(); // 隐藏默认的进度条样式
+BrowserProgress.hide(); // 隐藏默认的进度条样式
 
-progress.show(); // 显示默认的进度条样式
+BrowserProgress.show(); // 显示默认的进度条样式
 
 // 设置进程回调，会将进程的当前值作为参数传入回调
 // 当前值参数取值为 0~1 的浮点数
-progress.progress(callback);
+BrowserProgress.step(callback);
 
 // 淡出动画（用于自定义进度条）
 // 使 elem 元素在 duration 毫秒内淡出，duration 默认为 400
 // 如果 duration 为一个 function，则 duration 会被当做 callback 来处理，同时第三个参数 callback 会被忽略
-progress.fadeOut(elem, duration, callback);
+BrowserProgress.fadeOut(elem, duration, callback);
 ```
 
-如果使用 Ajax 更新页面内容，可以调用 .start() 方法重新开始进程，然后在 Ajax 的完成回调中调用 .done() 方法完成进程。
+如果使用 Ajax 更新页面内容，可以调用 `BrowserProgress.start()` 方法重新开始进程，然后在 Ajax 的完成回调中调用 `BrowserProgress.done()` 方法完成进程。
+
+
 
 ## Custom Progress
 
@@ -80,15 +85,15 @@ progress.fadeOut(elem, duration, callback);
 	</div>
 </div>
 
-<script src="progress.js"></script>
+<script src="BrowserProgress.js"></script>
 <script>
-	progress.hide(); // 隐藏掉原有的进度条样式
-	progress.progress(function(value) {
+	BrowserProgress.hide(); // 隐藏掉原有的进度条样式
+	BrowserProgress.progress(function(value) {
 		var percent = Math.round(value * 100) + '%';
 		document.getElementById('progress-text').innerHTML = percent;
 		document.getElementById('progress-bar').style.width = percent;
 		if (value == 1) {
-			progress.fadeOut(document.getElementById('progress-area'));
+			BrowserProgress.fadeOut(document.getElementById('progress-area'));
 		}
 	});
 </script>
--- dist/BrowserProgress.js
+++ dist/BrowserProgress.js
@@ -179,152 +179,146 @@ function css(elem, prop, value) {
 /***/ (function(module, exports, __webpack_require__) {
 
 /*!
- * mainLoop v1.0.2
- * (c) 2014-2017 BaiJunjie
+ * mainloop v1.0.3
+ * (c) 2014-2018 BaiJunjie
  * MIT Licensed.
  *
- * https://github.com/baijunjie/mainLoop.js
+ * https://github.com/baijunjie/mainloop
  */
 (function(root, factory) {
-	'use strict';
+    'use strict';
 
-	if (true) {
-		module.exports = factory();
-	} else {}
+    if (true) {
+        module.exports = factory();
+    } else {}
 
 }(this, function() {
-	'use strict';
+    'use strict';
 
-	var requestAnimationFrame = window.requestAnimationFrame,
-		cancelAnimationFrame = window.cancelAnimationFrame,
-		vendors = ['ms', 'moz', 'webkit', 'o'];
+    var requestAnimationFrame = window.requestAnimationFrame,
+        cancelAnimationFrame = window.cancelAnimationFrame,
+        vendors = ['ms', 'moz', 'webkit', 'o'];
 
-	for (var x = 0; x < vendors.length && !requestAnimationFrame; ++x) {
-		requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-		cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
-	}
+    for (var x = 0; x < vendors.length && !requestAnimationFrame; ++x) {
+        requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
+        cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    }
 
-	if (!cancelAnimationFrame) {
-		cancelAnimationFrame = function(id) {
-			window.clearTimeout(id);
-		};
-	}
+    if (!cancelAnimationFrame) {
+        cancelAnimationFrame = function(id) {
+            window.clearTimeout(id);
+        };
+    }
 
-	function Loop() {
-		this._requestAnimationFrame = requestAnimationFrame;
+    function Loop() {
+        this._requestAnimationFrame = requestAnimationFrame;
 
-		if (!this._requestAnimationFrame) {
-			var lastTime = 0;
-			this._requestAnimationFrame = function(callback) {
-				var currTime = new Date().getTime(),
-					timeToCall = Math.max(0, 16 - (currTime - lastTime)),
-					id = window.setTimeout(function() {
-						callback(currTime + timeToCall);
-					}, timeToCall);
-				lastTime = currTime + timeToCall;
-				return id;
-			};
-		}
+        if (!this._requestAnimationFrame) {
+            var lastTime = 0;
+            this._requestAnimationFrame = function(callback) {
+                var currTime = new Date().getTime(),
+                    timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+                    id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    }, timeToCall);
+                lastTime = currTime + timeToCall;
+                return id;
+            };
+        }
 
-		this._runFunc = []; //储存所有循环中运行的方法
-		this._active = !!this._runFunc.length; //表示循环是否在激活状态。当循环中存在需要运行的方法时，则被判定为激活状态
-		this._isRun = true; //表示循环是否在运行状态
-		this._timerID = 0;
-		this._loopCallback = proxy(this._loopCallback, this);
-	}
+        this._runFunc = []; //储存所有循环中运行的方法
+        this._active = !!this._runFunc.length; //表示循环是否在激活状态。当循环中存在需要运行的方法时，则被判定为激活状态
+        this._isRun = true; //表示循环是否在运行状态
+        this._timerID = 0;
+        this._loopCallback = proxy(this._loopCallback, this);
+    }
 
-	Loop.prototype = {
-		_loopCallback: function() {
-			var i = this._runFunc.length;
-			while (i--) {
-				if (this._runFunc[i]() === false) { // 如果方法返回 false，则将该方法移出循环
-					this._runFunc.splice(i, 1);
-				}
-			}
+    Loop.prototype = {
+        _loopCallback: function() {
+            var i = this._runFunc.length;
+            while (i--) {
+                if (this._runFunc[i]() === false) { // 如果方法返回 false，则将该方法移出循环
+                    this._runFunc.splice(i, 1);
+                }
+            }
 
-			if (this._runFunc.length) {
-				this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
-			} else {
-				this._active = false;
-			}
-		},
+            if (this._runFunc.length) {
+                this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
+            } else {
+                this._active = false;
+            }
+        },
 
-		add: function(func, target) {
-			if (typeof func !== 'function') return this;
-			if (target) {
-				func = proxy(func, target);
-			}
-			this._runFunc.unshift(func);
+        add: function(func, target) {
+            if (typeof func !== 'function') return this;
+            if (target) {
+                func = proxy(func, target);
+            }
+            this._runFunc.unshift(func);
 
-			if (this._active) return this;
-			this._active = true;
-			if (this._isRun) {
-				this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
-			}
-			return this;
-		},
+            if (this._active) return this;
+            this._active = true;
+            if (this._isRun) {
+                this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
+            }
+            return this;
+        },
 
-		remove: function(func) {
-			if (typeof func !== 'function') {
-				this._runFunc = [];
-				return this;
-			}
-			var i = this._runFunc.length;
-			while (i--) {
-				if (this._runFunc[i].guid === func.guid) {
-					this._runFunc.splice(i, 1);
-					return this;
-				}
-			}
-		},
+        remove: function(func) {
+            if (typeof func !== 'function') {
+                this._runFunc = [];
+                return this;
+            }
+            var i = this._runFunc.length;
+            while (i--) {
+                if (this._runFunc[i].guid === func.guid) {
+                    this._runFunc.splice(i, 1);
+                    return this;
+                }
+            }
+        },
 
-		stop: function() { // 停止循环
-			if (!this._isRun) return this;
-			this._isRun = false;
-			cancelAnimationFrame(this._timerID);
-			return this;
-		},
+        stop: function() { // 停止循环
+            if (!this._isRun) return this;
+            this._isRun = false;
+            cancelAnimationFrame(this._timerID);
+            return this;
+        },
 
-		play: function() { // 启动循环
-			if (this._isRun) return this;
-			this._isRun = true;
-			if (this._active) {
-				this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
-			}
-			return this;
-		}
-	};
+        play: function() { // 启动循环
+            if (this._isRun) return this;
+            this._isRun = true;
+            if (this._active) {
+                this._timerID = this._requestAnimationFrame.call(window, this._loopCallback);
+            }
+            return this;
+        }
+    };
 
-	var guid = 0;
-	function proxy(func, target) {
-		if (typeof target === 'string') {
-			var tmp = func[target];
-			target = func;
-			func = tmp;
-		}
+    var guid = 0;
+    function proxy(func, target) {
+        if (typeof target === 'string') {
+            var tmp = func[target];
+            target = func;
+            func = tmp;
+        }
 
-		if (typeof func !== 'function') {
-			return undefined;
-		}
+        if (typeof func !== 'function') {
+            return undefined;
+        }
 
-		var slice = Array.prototype.slice,
-			args = slice.call(arguments, 2),
-			proxy = function() {
-				return func.apply(target || this, args.concat(slice.call(arguments)));
-			};
+        var slice = Array.prototype.slice,
+            args = slice.call(arguments, 2),
+            proxy = function() {
+                return func.apply(target || this, args.concat(slice.call(arguments)));
+            };
 
-		proxy.guid = func.guid = func.guid || guid++;
+        proxy.guid = func.guid = func.guid || guid++;
 
-		return proxy;
-	}
+        return proxy;
+    }
 
-	var loop = new Loop();
-	loop.createLoop = function() {
-		return new Loop();
-	};
-
-	return loop;
-
+    return new Loop();
 }));
 
 
